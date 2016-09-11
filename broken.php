ae336367efcd4d282f978bc328e01c5b277ff576
RahulLatino <mapleuniversalmaple@gmail.com>
2016-09-10 20:01:53 -0500

    Add files via upload

---
+++ broken.php
@@ -0,0 +1,59 @@
+<?php 
+
+/*¯¯¯¯|¯¯¯¯¯|¯¯¯¯¯¯¯¯|¯¯¯|¯¯¯||¯¯¯|¯¯¯|¯¯¯¯¯¯¯¯|¯¯¯¯¯¯|¯¯¯¯*\
+|###########################################|
+|#_____________________..ButsagsCMS.._____________________#|
+|#______.Copyright ©2000-2009 Chris Ingis. All Rights Reserved._____#|
+|###########################################|
+\*____|_____|________|___|___||___|___|________|______|____*/
+
+
+echo '
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>.:Server Offline:.</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta http-equiv="content-language" content="en" />
+<link href="broken.css" rel="stylesheet" type="text/css" />
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+</head>
+
+<body >
+<img src="install/images/logo.png" style= position:top-left; />
+<table class="w100 h100">
+	<tr>
+		<td><center>
+			<table class="outercontainer">
+				<tr>
+					<td style="padding:3px">
+						<div class="innercontainer">
+							<h1><center><font color=black>Server Error</font></center></h1>
+								<div valign="middle">
+									<div style="color: white;">
+                                       <h4><center><font color=red>Server Offline.</font></center></h4>
+					                     <hr />
+					                   <ol>
+									     <b>
+									       <center><font color=black>
+						No connection to database. Server host is currently unreachable.<br>
+						Please try again later. <br>Sorry for any inconvenience.
+					                       </font>
+										   <embed wmode="transparent" src="http://www.xatech.com/web_gear/chat/chat.swf" quality="high" width="100%" height="405" name="chat" FlashVars="id=67967372" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://xat.com/update_flash.shtml" />
+										   </center>
+										 </b>
+									   </ol>
+									 </div>
+								   </div>
+								 </div>
+							   </td>
+							 </tr>
+						   </table>
+						 </center>
+					   </td>
+					 </tr>
+				   </table>
+			   </body>
+
+';
+?>
---
+++ index.php
@@ -0,0 +1,101 @@
+<?php 
+
+/*¯¯¯¯|¯¯¯¯¯|¯¯¯¯¯¯¯¯|¯¯¯|¯¯¯||¯¯¯|¯¯¯|¯¯¯¯¯¯¯¯|¯¯¯¯¯¯|¯¯¯¯*\
+|###########################################|
+|#_____________________..ButsagsCMS.._____________________#|
+|#______.Copyright ©2000-2016 Chris Ingis. All Rights Reserved._____#|
+|###########################################|
+\*____|_____|________|___|___||___|___|________|______|____*/
+
+
+# Begin Session
+session_start();
+
+# Disable Notices
+error_reporting(E_ALL ^ E_NOTICE);
+
+# Begin GZip Compression
+if(extension_loaded('zlib')){
+	ini_set('zlib.output_compression_level', 1);  
+	ob_start('ob_gzhandler'); 
+}
+
+# Is ButsagsCMS installed?
+if(!file_exists('install/butsags.lock')){
+	header("Location: install/install.php");
+	exit;
+} else {
+	# Stable Version Number
+	define("ButsagsCMS_VERSION", "0.0.1");
+
+	# Import Database Driver
+	require_once("config/database.php");
+
+	# Import Essential Files
+	require_once("config/properties.php");
+	require_once("config/functions.php");
+
+	# Define $getbutsags variable
+	$getbutsags = isset($_GET['butsags']) ? $_GET['butsags'] : "";
+
+	switch($getbutsags){
+		case NULL:
+			header('Location: ?butsags=main');
+			break;
+		case "main":
+			include($styledir."/header.php");
+			include("includes/public/main.php");
+			include($styledir."/footer.php");
+			break;
+		case "ucp":
+			include($styledir."/header.php");
+			include("includes/ucp/main.php");
+			include($styledir."/footer.php");
+			break;
+		case "admin":
+			include($styledir."/header.php");
+			include("includes/admin/main.php");
+			include($styledir."/footer.php");
+			break;
+		case "gmcp":
+			include($styledir."/header.php");
+			include("includes/gmcp/main.php");
+			include($styledir."/footer.php");
+			break;
+		case "bbs":
+			include($styledir."/header.php");
+			include("includes/bbs/main.php");
+			include($styledir."/footer.php");
+			break;
+		case "misc":
+			include("includes/misc/main.php");
+			break;
+		case "style":
+			include($styledir."/header.php");
+			include("includes/public/styles.php");
+			include($styledir."/footer.php");
+			break;
+		case "cursor":
+			include($styledir."/header.php");
+			include("includes/public/cursors.php");
+			include($styledir."/footer.php");
+			break;
+		case "butsagsdl":
+			include("includes/misc/phpdownload.php");
+			break;
+		default:
+			include($styledir."/header.php");
+			include("includes/public/main.php");
+			include($styledir."/footer.php");
+			break;
+		case "info":
+			include($styledir."/header.php");
+			include("includes/public/info.php");
+			include($styledir."/footer.php");
+			break;
+
+	}
+}
+
+$mysqli->close();
+?>
